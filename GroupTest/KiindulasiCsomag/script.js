9fe8c5b56f9f1135adecb4978aaef6139d2aec38
Zoran Heinczinger <zoran.heinczinger@gmail.com>
2023-10-11 17:49:04 +0200

    publish grouptest

---
+++ GroupTest/KiindulasiCsomag/script.js
@@ -0,0 +1,8 @@
+// Jó munkát!
+
+function delegate(parent, type, selector, handler) {
+    parent.addEventListener(type, function (event) {
+        const targetElement = event.target.closest(selector)
+        if (this.contains(targetElement)) handler.call(targetElement, event)
+    })
+}
